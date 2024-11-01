0a54fe414184064fd1788545e3bfad7d38941b01
Felipe-Kunst <Felipe-Kunst@outlook.com>
2024-11-01 14:23:15 -0300

    Adicionando adição de tarefa via upload de arquivo Csv

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const app = express();
+const csv = require('csv-parser');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 const connection = require('./database/connection');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-app.use(express.static("public"));
-app.set('view engine', 'ejs')
+const app = express();
+const upload = multer({ dest: 'upload/' });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   connection.query("SELECT * FROM tarefa", (err, results) => {
@@ -14,66 +19,88 @@ app.get('/', (req, res) => {
       console.error("Erro ao executar a consulta:", err);
       return res.status(500).send("Erro no servidor");
     }
-    res.render("index", { tarefas : results })
-  })
+    res.render("index", { tarefas: results });
+  });
 });
 
-
-app.get('/deletar/:id', (req, res) =>{
-    let id = req.params.id
-    connection.query('delete from tarefa where tarefa_id = ?', [id], (err, results) => {
-        if (err) throw err
-        res.redirect('/')
-    })
-})
+app.get('/deletar/:id', (req, res) => {
+  let id = req.params.id;
+  connection.query('DELETE FROM tarefa WHERE tarefa_id = ?', [id], (err, results) => {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
 
 app.get('/create', (req, res) => {
-    res.render('create')
-})
-
-app.post('/create', (req, res) => {
-    let {titulo, descricao, data_criacao, data_finalizacao} = req.body
-    connection.query('insert into tarefa(titulo, descricao, data_criacao, data_finalizacao) values (?,?,?,?)'
-        , [titulo, descricao, data_criacao, data_finalizacao]
-        , (err, result) => {
-            if(err) throw err
-            res.redirect('/')
-        })
-
-})
-
-app.get('/edit/:id', (req, res)=> {
-    let id = req.params.id;
-    connection.query('select * from tarefa where tarefa_id = ?', [id], (err, results) => {
-        if (err) {
-          console.error("Erro ao buscar a tarefa:", err);
-          return res.status(500).send("Erro no servidor");
-        }
-
-        if (results.length === 0) {
-          return res.status(404).send("Tarefa não encontrada");
-        }
-
-        res.render('edit', { tarefa: results[0] });
-    });
-
-})
-
-app.post('/edit/:id', (req, res) => {
-    let id = parseInt(req.params.id)
-    let {titulo, descricao, data_criacao, data_finalizacao} = req.body
-    connection.query(
-      'update tarefa set titulo = ?, descricao = ?, data_criacao = ?, data_finalizacao = ? where tarefa_id = ?'
-      , [titulo, descricao, data_criacao, data_finalizacao, id],
-      (err, result) => {
-        if (err) throw err;
-        console.log("Tarefa atualizada com sucesso!");
-        res.redirect('/');
-      }
-    );
+  res.render('create');
 });
 
+app.post('/create', (req, res) => {
+  let { titulo, descricao, data_criacao, data_finalizacao } = req.body;
+  connection.query(
+    'INSERT INTO tarefa (titulo, descricao, data_criacao, data_finalizacao) VALUES (?, ?, ?, ?)',
+    [titulo, descricao, data_criacao, data_finalizacao],
+    (err, result) => {
+      if (err) throw err;
+      res.redirect('/');
+    }
+  );
+});
+
+app.get('/edit/:id', (req, res) => {
+  let id = req.params.id;
+  connection.query('SELECT * FROM tarefa WHERE tarefa_id = ?', [id], (err, results) => {
+    if (err) {
+      console.error("Erro ao buscar a tarefa:", err);
+      return res.status(500).send("Erro no servidor");
+    }
+
+    if (results.length === 0) {
+      return res.status(404).send("Tarefa não encontrada");
+    }
+
+    res.render('edit', { tarefa: results[0] });
+  });
+});
+
+app.post('/edit/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let { titulo, descricao, data_criacao, data_finalizacao } = req.body;
+  connection.query(
+    'UPDATE tarefa SET titulo = ?, descricao = ?, data_criacao = ?, data_finalizacao = ? WHERE tarefa_id = ?',
+    [titulo, descricao, data_criacao, data_finalizacao, id],
+    (err, result) => {
+      if (err) throw err;
+      console.log("Tarefa atualizada com sucesso!");
+      res.redirect('/');
+    }
+  );
+});
+
+app.post('/upload-csv', upload.single('file'), (req, res) => {
+  const filepath = req.file.path;
+  const results = [];
+
+  fs.createReadStream(filepath)
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      fs.unlinkSync(filepath);
+
+      const sql = 'INSERT INTO tarefa (titulo, descricao, data_criacao,data_finalizacao) VALUES ?';
+      const values = results.map(item => [item.titulo, item.descricao, item.data_criacao,item.data_finalizacao]);
+
+      connection.query(sql, [values], (err) => {
+        if (err) throw err;
+        res.redirect('/');
+      });
+    });
+});
+
+app.get('/upload-csv', (req, res) => {
+  res.render('upload-csv');
+});
 
 app.listen(3007, () => {
-  console.log("executando...");
+  console.log("Servidor executando na porta 3007...");
 });
